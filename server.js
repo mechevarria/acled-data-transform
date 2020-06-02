309345ab5f8d04c3d3d8b605f31d31ba926c4305
Mike Echevarria <mike.echevarria@gmail.com>
2020-06-02 14:12:19 -0400

    finish data transformation and writing

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 const os = require('os');
 const AdmZip = require('adm-zip');
 const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify/lib/sync');
 
 const zipFile = './acled.zip';
 const zip = new AdmZip(zipFile);
-const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'acled-'));
 
-console.info(`extracting ${zipFile} to ${tempDir}`);
-zip.extractAllTo(tempDir, true);
+console.info(`extracting ${zipFile} to ${os.tmpdir()}`);
+zip.extractAllTo(os.tmpdir(), true);
 
 const csvFile = 'acled.csv';
-const content = fs.readFileSync(`${tempDir}/${csvFile}`);
+const content = fs.readFileSync(`${os.tmpdir()}/${csvFile}`);
 const records = parse(content, {
     columns: true,
-    skip_empty_lines: true
+    skip_empty_lines: true,
+    to_line: 300000,
+    on_record: (record) => {
+        record['geolocation'] = `POINT (${record['longitude']} ${record['latitude']})`;
+
+        delete record['data_id'];
+        delete record['iso'];
+        delete record['event_id_no_cnty'];
+        delete record['time_precision'];
+        delete record['inter1'];
+        delete record['inter2'];
+        delete record['interaction'];
+        delete record['soruce_scale'];
+        delete record['geo_precision'];
+        delete record['iso3'];
+        delete record['source_scale'];
+        delete record['admin1'];
+        delete record['admin2'];
+        delete record['admin3'];
+
+        return record;
+    }
 });
 
 console.info(`${csvFile} has ${records.length} records`);
 
+fs.writeFileSync('event.csv', stringify(records, {
+    header: true
+}));
+
 console.info(`Deleting ${csvFile}`);
-fs.unlinkSync(`${tempDir}/${csvFile}`);
-console.info(`Deleting ${tempDir}`);
-fs.rmdirSync(tempDir);
+fs.unlinkSync(`${os.tmpdir()}/${csvFile}`);
